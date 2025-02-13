97cca8f97cd724b8892041b5ebc01b02c398c2e4
arcabob <159237376+arca-bob@users.noreply.github.com>
2025-02-13 12:44:00 +0100

    Add files via upload

---
+++ FormComponent.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import ButtonComponent from './ButtonComponent.vue';
+import FieldsComponent from './FieldsComponent.vue';
+
+interface FormComponentProperties {
+    data:{
+        fields: Array<{id: string, placeholder: string, class?: string, type?: string, name: string}>
+        buttons: {id: string, type?: 'button' | 'submit' | 'reset', class: string }[]
+    }
+}
+
+defineProps<FormComponentProperties>()
+
+</script>
+
+<template>
+    <main class="login_main">
+        <form class="custom-form">
+            <section class="custom-form_fields-section">
+               <FieldsComponent 
+               v-for="(field,index) in data.fields"
+               :key="index"
+               :data="field"
+               /> 
+            </section>
+                <ButtonComponent
+                    v-for="(button,index) in data.buttons"
+                    :key="index"
+                    :data="button"
+                />
+
+        </form>
+    </main>
+
+</template>
